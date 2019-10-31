66447166fe6db1e9af87057f084417d0fb0cbc20
KatrinSmol <43372421+KatrinSmol@users.noreply.github.com>
2019-10-31 13:32:03 +0300

    automated student and groups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,52 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Group.destroy_all
+User.destroy_all
+Student.destroy_all
+puts "Everything just destroyed"
+
+groups = [
+  {
+    start_year: 2018,
+    end_year:  2022,
+    codename: "HSEADC2018",
+    name: "Третий отряд"
+  }, {
+    start_year: 2017,
+    end_year:  2021,
+    codename: "HSEADC2017",
+    name: "Ночь нежна"
+  }, {
+    end_year:  2020,
+    codename: "HSEADC2016",
+    name: "Поток 2020"
+  }, {
+    start_year: 2019,
+    end_year:  2023,
+    codename: "HSEADC2019 G19",
+    name: "HSEADC2019 G19"
+  }, {
+    start_year: 2019,
+    end_year:  2023,
+    codename: "HSEADC2019 G20",
+    name: "HSEADC2019 G20"
+  }
+]
+
+groups.each do |group|
+  g = Group.create(group)
+  puts "New group just was created with id #{ g.id } with name #{ g.name }"
+end
+
+user = User.create!(
+  email: 'katrinsmol@mail.ru',
+  password: 'testtest'
+)
+
+puts "User with email #{ user.email } just created"
+
+student = Student.find_by(user_id: user.id)
+student.first_name = "Katrin"
+student.last_name = "Smol"
+
+student.save!
+
+puts "Student updated"
